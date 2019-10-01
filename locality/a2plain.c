b559cfbe9cb34e9f58e9ed55176065d1a518be3f
Camille <camille.calabrese@gmail.com>
2019-10-01 19:48:15 -0400

    downloaded from halligan

---
+++ locality/a2plain.c
@@ -0,0 +1,83 @@
+#include <string.h>
+
+#include <a2plain.h>
+#include "uarray2.h"
+
+/************************************************/
+/* Define a private version of each function in */
+/* A2Methods_T that we implement.               */
+/************************************************/
+
+static A2Methods_UArray2 new(int width, int height, int size)
+{
+  //TODO: Implement this function and remove the dummy return statement.
+  return NULL;
+}
+
+static A2Methods_UArray2 new_with_blocksize(int width, int height, int size, int blocksize)
+{
+  //TODO: Implement this function and remove the dummy return statement.
+  (void) blocksize;
+  return NULL;
+}
+
+
+/* TODO: ...many more private (static) definitions follow */
+
+static void map_row_major(A2Methods_UArray2 uarray2,
+                          A2Methods_applyfun apply,
+                          void *cl)
+{
+  UArray2_map_row_major(uarray2, (UArray2_applyfun*)apply, cl);
+}
+
+static void map_col_major(A2Methods_UArray2 uarray2,
+                          A2Methods_applyfun apply,
+                          void *cl)
+{
+  UArray2_map_col_major(uarray2, (UArray2_applyfun*)apply, cl);
+}
+
+struct small_closure {
+  A2Methods_smallapplyfun *apply; 
+  void                    *cl;
+};
+
+static void apply_small(int i, int j, UArray2_T uarray2,
+                        void *elem, void *vcl)
+{
+  struct small_closure *cl = vcl;
+  (void)i;
+  (void)j;
+  (void)uarray2;
+  cl->apply(elem, cl->cl);
+}
+
+static void small_map_row_major(A2Methods_UArray2        a2,
+                                A2Methods_smallapplyfun  apply,
+                                void *cl)
+{
+  struct small_closure mycl = { apply, cl };
+  UArray2_map_row_major(a2, apply_small, &mycl);
+}
+
+static void small_map_col_major(A2Methods_UArray2        a2,
+                                A2Methods_smallapplyfun  apply,
+                                void *cl)
+{
+  struct small_closure mycl = { apply, cl };
+  UArray2_map_col_major(a2, apply_small, &mycl);
+}
+
+
+static struct A2Methods_T uarray2_methods_blocked_struct = {
+  new,
+  new_with_blocksize,
+  /* ... other functions follow in order,
+   *     with NULL for those not implemented ...
+   */
+};
+
+// finally the payoff: here is the exported pointer to the struct
+
+A2Methods_T uarray2_methods_blocked = &uarray2_methods_blocked_struct;
---
+++ locality/ppmtrans.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "assert.h"
+#include "a2methods.h"
+#include "a2plain.h"
+#include "a2blocked.h"
+#include "pnm.h"
+
+#define SET_METHODS(METHODS, MAP, WHAT) do {                    \
+        methods = (METHODS);                                    \
+        assert(methods != NULL);                                \
+        map = methods->MAP;                                     \
+        if (map == NULL) {                                      \
+                fprintf(stderr, "%s does not support "          \
+                                WHAT "mapping\n",               \
+                                argv[0]);                       \
+                exit(1);                                        \
+        }                                                       \
+} while (0)
+
+static void
+usage(const char *progname)
+{
+        fprintf(stderr, "Usage: %s [-rotate <angle>] "
+                        "[-{row,col,block}-major] [filename]\n",
+                        progname);
+        exit(1);
+}
+
+int main(int argc, char *argv[]) 
+{
+        char *time_file_name = NULL;
+        int   rotation       = 0;
+        int   i;
+
+        /* default to UArray2 methods */
+        A2Methods_T methods = uarray2_methods_plain; 
+        assert(methods);
+
+        /* default to best map */
+        A2Methods_mapfun *map = methods->map_default; 
+        assert(map);
+
+        for (i = 1; i < argc; i++) {
+                if (strcmp(argv[i], "-row-major") == 0) {
+                        SET_METHODS(uarray2_methods_plain, map_row_major, 
+				    "row-major");
+                } else if (strcmp(argv[i], "-col-major") == 0) {
+                        SET_METHODS(uarray2_methods_plain, map_col_major, 
+				    "column-major");
+                } else if (strcmp(argv[i], "-block-major") == 0) {
+                        SET_METHODS(uarray2_methods_blocked, map_block_major,
+                                    "block-major");
+                } else if (strcmp(argv[i], "-rotate") == 0) {
+                        if (!(i + 1 < argc)) {      /* no rotate value */
+                                usage(argv[0]);
+                        }
+                        char *endptr;
+                        rotation = strtol(argv[++i], &endptr, 10);
+                        if (!(rotation == 0 || rotation == 90 ||
+                            rotation == 180 || rotation == 270)) {
+                                fprintf(stderr, 
+					"Rotation must be 0, 90 180 or 270\n");
+                                usage(argv[0]);
+                        }
+                        if (!(*endptr == '\0')) {    /* Not a number */
+                                usage(argv[0]);
+                        }
+                } else if (strcmp(argv[i], "-time") == 0) {
+                        time_file_name = argv[++i];
+                } else if (*argv[i] == '-') {
+                        fprintf(stderr, "%s: unknown option '%s'\n", argv[0],
+				argv[i]);
+                } else if (argc - i > 1) {
+                        fprintf(stderr, "Too many arguments\n");
+                        usage(argv[0]);
+                } else {
+                        break;
+                }
+        }
+
+        assert(0);              // the rest of this function is not yet implemented
+}
